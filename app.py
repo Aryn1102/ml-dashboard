88f83b2206f10b1c8a1e16acf012461cc52fb337
Aryn1102 <rajaryan3132@gmail.com>
2025-08-15 15:20:47 +0530

    Upload table

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My ML-Dashboard")
 st.write("Welcome to the app! This is where your journey begins.")
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']}
+st.dataframe(pd.DataFrame(data))
