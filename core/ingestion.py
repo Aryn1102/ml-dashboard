b2ac1c7574e02b0a2e9e1b8e89ba6765fec9b237
Raj Aryan <rajaryan3132@gmail.com>
2026-04-09 19:57:05 +0530

    added download Csv file from URL

--- core/ingestion.py
+++ core/ingestion.py
@@ -1,6 +1,32 @@
 import pandas as pd
+import requests
+from io import StringIO
+
+def download_data(url:str):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise ValueError(f"Failed to fetch data. Status code: {response.status_code}")
+
+    try:
+        data = StringIO(response.text)
+        df = pd.read_csv(data)
+    except Exception as e:
+        raise ValueError(f"Error reading CSV: {e}")
+
+    return df
 
 def load_data(path:str):
-    df=pd.read_csv(path)
-    df=df.dropna(how="all")
+    try:
+        df = pd.read_csv(path)
+    except Exception as e:
+        raise ValueError(f"Error loading file: {e}")
+
+    df = df.dropna(how="all")
+
+    if df.empty:
+        raise ValueError("Loaded DataFrame is empty")
+
     return df
