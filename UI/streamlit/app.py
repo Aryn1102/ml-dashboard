6540522595c85691d38831f3cf689c531191a5d5
Raj Aryan <rajaryan3132@gmail.com>
2026-09-13 13:33:12 +0530

    add no of duplicates to app.py

--- UI/streamlit/app.py
+++ UI/streamlit/app.py
@@ -2,36 +2,63 @@ import streamlit as st
 from backend.factory import LoaderFactory
 from backend.data_manager import DataManager
 
-st.title("Universal Data Dashboard")
+st.title("DataFrame Only Test")
 
 uploaded_file = st.file_uploader("Upload Dataset")
 
 if uploaded_file is not None:
-    if 'manager' not in st.session_state or uploaded_file.name != st.session_state.file_name:
+
+    if (
+        "manager" not in st.session_state
+        or uploaded_file.name != st.session_state.file_name
+    ):
         loader = LoaderFactory.create_loader(uploaded_file)
         manager = DataManager(loader)
         manager.load()
+
         st.session_state.manager = manager
         st.session_state.file_name = uploaded_file.name
 
-    st.dataframe(st.session_state.manager.data)
+    manager = st.session_state.manager
+    data = manager.data
+
+    st.subheader("Dataset Preview")
+    st.dataframe(data)
 
     st.subheader("Dataset Shape")
-    st.write(st.session_state.manager.analyzer.shape())
+    row, col = manager.analyzer.shape()
+    st.metric("Rows", row)
+    st.metric("Columns", col)
+
     st.subheader("Columns")
-    st.write(st.session_state.manager.analyzer.columns())
-    st.subheader("Missing values")
-    st.write(st.session_state.manager.analyzer.missing_values())
-    st.subheader("Datatypes")
-    st.write(st.session_state.manager.analyzer.dtypes())
-    st.subheader("Duplicates")
-    st.write(st.session_state.manager.analyzer.duplicates())
-    if st.button("Normalize Missing Values"):
-        st.session_state.manager.cleaner.normalize_missing_values()
-        st.success("Missing-value representations normalized.")
-    if st.button("Remove Duplicates"):
-        before = st.session_state.manager.analyzer.shape()[0]
-        st.session_state.manager.cleaner.remove_duplicates()
-        after = st.session_state.manager.analyzer.shape()[0]
-        st.success(f"Removed {before - after} duplicate rows.")
+    columns = manager.analyzer.columns()
+    st.dataframe(
+        columns.to_frame(name="Column"),
+        hide_index=True
+    )
+
+    st.subheader("Missing Values")
+
+    missing_values = manager.analyzer.missing_values().to_frame(
+        name="Missing Values"
+    ).reset_index()
+
+    missing_values = missing_values.rename(
+        columns={"index": "Column"}
+    )
+
+    st.dataframe(
+        missing_values,
+        hide_index=True
+    )
     
+    st.subheader("Datatypes")
+    dtypes = manager.analyzer.dtypes().astype(str).to_frame(name="Dtype").reset_index()
+    dtypes = dtypes.rename(columns={"index": "Column"})
+    st.dataframe(
+        dtypes,
+        hide_index=True
+    )
+    st.subheader("Duplicate")
+    duplicates_shape = manager.analyzer.duplicates().shape
+    st.metric("Duplicate Rows", duplicates_shape[0])
